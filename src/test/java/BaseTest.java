bc61596757906fa2fa2eff8213f64ec4188f0b54
Diego MG <madi@erni-espana.es>
2022-02-17 20:47:27 +0100

    add base

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseTest {
+
+    private WebDriver driver;
+
+    @BeforeSuite
+    public void beforeSuite() {
+        System.setProperty("webdriver.chrome.driver","C:\\temp\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        if(null != driver) {
+            driver.close();
+            driver.quit();
+        }
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
--- src/test/java/TestingClass.java
+++ src/test/java/TestingClass.java
@@ -1,29 +1,12 @@
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TestingClass {
-
-    private WebDriver driver;
-
-    @Before
-    public void beforeTest()
-    {
-        System.setProperty("webdriver.chrome.driver","C:\\temp\\chromedriver.exe");
-        driver = new ChromeDriver();
-    }
-
-    @After
-    public void afterTest()
-    {
-        driver.quit();
-    }
+public class TestingClass extends BaseTest {
 
     @Test
     public void testBase()
     {
+        WebDriver driver = getDriver();
         driver.get("https://www.google.com");
     }
 }
